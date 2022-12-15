1801ba32dbead7f1be4267260172f21c96143661
Gabriel Bertrand <gabriel_bertrand@hotmail.com>
2022-12-15 12:47:16 -0500

    ajout de blablabla

---
+++ Tp3JV/Assets/Script/TopDownCharacterControllerWithDust.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cainos.PixelArtTopDown_Basic
+{
+    public class TopDownCharacterControllerWithDust : MonoBehaviour
+    {
+        public float speed;
+
+        private Animator animator;
+        private Rigidbody2D rigidbody;
+        private ParticleSystem dust;
+
+        private Vector2 lastDir;
+
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+            rigidbody = GetComponent<Rigidbody2D>();
+            dust = GetComponentInChildren<ParticleSystem>();
+        }
+
+
+        private void Update()
+        {
+            Vector2 dir = Vector2.zero;
+            if (Input.GetKey(KeyCode.A))
+            {
+                dir.x = -1;
+                animator.SetInteger("Direction", 3);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                dir.x = 1;
+                animator.SetInteger("Direction", 2);
+            }
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                dir.y = 1;
+                animator.SetInteger("Direction", 1);
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                dir.y = -1;
+                animator.SetInteger("Direction", 0);
+            }
+
+            dir.Normalize();
+
+            if (lastDir != dir && dir != Vector2.zero)
+                CreateDust();
+            lastDir = dir;
+
+            animator.SetBool("IsMoving", dir.magnitude > 0);
+
+            rigidbody.velocity = speed * dir;
+        }
+
+        private void CreateDust()
+        {
+            dust.Play();
+        }
+    }
+}
